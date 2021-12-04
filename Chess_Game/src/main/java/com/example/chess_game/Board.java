ffe3a03c9ecbd37b84cfea892c33e6f950fcf61d
Miguel Arcilla <miguelkristopharcilla@gmail.com>
2021-12-04 15:24:33 +0100

    Added classes Field and Board + Board init

---
+++ Chess_Game/src/main/java/com/example/chess_game/Board.java
@@ -0,0 +1,26 @@
+package com.example.chess_game;
+
+public class Board {
+
+    private Field[][] fields;
+    private static final int ASCII_OFFSET = 97;
+
+    public Board() {
+        initFields();
+    }
+
+    private void initFields() {
+        this.fields = new Field[8][8];
+        // Starting with field a8 top-left
+        for (int rowNum = 7; rowNum >= 0; rowNum--) {
+            for (int colAlphabet = 0; colAlphabet < 8; colAlphabet++) {
+                this.fields[7 - rowNum][colAlphabet]
+                        = new Field((char) (colAlphabet + ASCII_OFFSET) + Integer.toString(rowNum + 1));
+            }
+        }
+    }
+
+    public Field[][] getFields() {
+        return this.fields;
+    }
+}
---
+++ Chess_Game/src/main/java/com/example/chess_game/Field.java
@@ -0,0 +1,17 @@
+package com.example.chess_game;
+
+public class Field {
+    private final String fieldName;
+    // Todo
+    // UI-Coordinates
+    // Piece
+
+    public Field(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+}
