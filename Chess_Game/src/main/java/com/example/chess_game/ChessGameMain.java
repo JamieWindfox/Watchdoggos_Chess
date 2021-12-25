38e297d15094a0fa6ad4f08836c97763ac775d8e
Jamie <74956250+JamieWindfox@users.noreply.github.com>
2021-12-25 17:53:58 +0100

    Added Previous Moves Button and ordered the buttons in the main stage

--- Chess_Game/src/main/java/com/example/chess_game/ChessGameMain.java
+++ Chess_Game/src/main/java/com/example/chess_game/ChessGameMain.java
@@ -2,9 +2,11 @@ package com.example.chess_game;
 
 import guiElements.NewGameButton;
 import guiElements.ResignGameButton;
+import guiElements.ShowPreviousMovesButton;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -15,12 +17,18 @@ public class ChessGameMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        HBox hBox = new HBox();
+        GridPane pane = new GridPane();
         Button newGameButton = new NewGameButton();
         Button resignGameButton = new ResignGameButton();
-        hBox.getChildren().addAll(newGameButton, resignGameButton);
 
-        Scene scene = new Scene(hBox, 320, 240);
+        HBox bottomPanel = new HBox();
+        Button prevMovesButton = new ShowPreviousMovesButton();
+
+        pane.add(newGameButton, 0, 0);
+        pane.add(resignGameButton, 1, 0);
+        pane.add(prevMovesButton, 2, 2);
+
+        Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Watchdoggos Chess!");
         stage.setScene(scene);
         stage.show();
