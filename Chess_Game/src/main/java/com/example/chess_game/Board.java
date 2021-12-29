d5b3f63b584405212bf27028ada5db2d87aaa8e0
Kuuko <miguelkristopharcilla@gmail.com>
2021-12-29 14:09:50 +0100

    Updated board logic

--- Chess_Game/src/main/java/com/example/chess_game/Board.java
+++ Chess_Game/src/main/java/com/example/chess_game/Board.java
@@ -1,26 +1,72 @@
 package com.example.chess_game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
 
-    private Field[][] fields;
+    private final Field[][] fields;
     private static final int ASCII_OFFSET = 97;
+    private final Map<Piece, Field> pieceLocation;
 
-    public Board() {
-        initFields();
+    public Board(Player white, Player black) {
+        this.fields = new Field[8][8];
+        this.pieceLocation = new HashMap<>();
+        initFields(white, black);
     }
 
-    private void initFields() {
-        this.fields = new Field[8][8];
-        // Starting with field a8 top-left
-        for (int rowNum = 7; rowNum >= 0; rowNum--) {
+    public void initFields(Player white, Player black) {
+        for (int rowNum = 0; rowNum < 8; rowNum++) {
             for (int colAlphabet = 0; colAlphabet < 8; colAlphabet++) {
-                this.fields[7 - rowNum][colAlphabet]
-                        = new Field((char) (colAlphabet + ASCII_OFFSET) + Integer.toString(rowNum + 1), colAlphabet, rowNum);
+
+                Field currentField
+                        = new Field((char) (colAlphabet + ASCII_OFFSET) + Integer.toString(rowNum + 1), rowNum, colAlphabet);
+
+                this.fields[rowNum][colAlphabet] = currentField;
+                if (rowNum == 6) {
+                    setPieceOnBoard(Pawn.class, Piece.BLACK, currentField, black);
+                }
+                if (rowNum == 1) {
+                    setPieceOnBoard(Pawn.class, Piece.WHITE, currentField, white);
+                }
             }
+        }
+    }
+
+    private void setPieceOnBoard(Class<? extends Piece> piece, int color, Field fieldToPlace, Player player) {
+        if (piece == Pawn.class) {
+            Pawn p = new Pawn(color);
+            fieldToPlace.setPiece(p);
+            pieceLocation.put(p, fieldToPlace);
+            player.addPiece(p);
         }
     }
 
     public Field[][] getFields() {
         return this.fields;
+    }
+
+    public Field getPieceLocation(Piece p) {
+        return pieceLocation.get(p);
+    }
+
+    public void printField() {
+        for (int rowNum = 0; rowNum < 8; rowNum++) {
+            for (int colAlphabet = 0; colAlphabet < 8; colAlphabet++) {
+                Field currentField = this.fields[7 - rowNum][colAlphabet];
+                System.out.printf("[%s] ", currentField.getPiece() != null ?
+                        currentField.getPiece().getName() + "_" + currentField.getPiece().getColor()
+                        : currentField.getFieldName());
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void update(Field newField, Piece piece) {
+        Field oldField = pieceLocation.remove(piece);
+        pieceLocation.put(piece, newField);
+        fields[oldField.getRow()][oldField.getColumn()].setPiece(null);
+        fields[newField.getRow()][newField.getColumn()].setPiece(piece);
     }
 }
--- Chess_Game/src/main/java/com/example/chess_game/Field.java
+++ Chess_Game/src/main/java/com/example/chess_game/Field.java
@@ -2,27 +2,35 @@ package com.example.chess_game;
 
 public class Field {
     private final String fieldName;
-    private int x;
-    private int y;
+    private final int row;
+    private final int column;
     // Todo
     // UI-Coordinates
-    // Piece
+    private Piece piece;
 
-    public Field(String fieldName, int x, int y) {
+    public Field(String fieldName, int row, int column) {
         this.fieldName = fieldName;
-        this.x = x;
-        this.y = y;
+        this.row = row;
+        this.column = column;
     }
 
     public String getFieldName() {
         return fieldName;
     }
 
-    public int getX() {
-        return x;
+    public int getRow() {
+        return row;
     }
 
-    public int getY() {
-        return y;
+    public int getColumn() {
+        return column;
+    }
+
+    public Piece getPiece() {
+        return this.piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
     }
 }
